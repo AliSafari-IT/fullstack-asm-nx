b86f231f67aae01dde15ef989e02bc5509ba5591
asafarim <asafarim.it@gmail.com>
2024-11-19 02:08:29 +0100

    connected to mysql db

--- apps/ShieldAccess/ShieldAccess.AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var apiService = builder.AddProject<Projects.ShieldAccess_ApiService>("apiservice");
-
-builder.AddProject<Projects.ShieldAccess_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
-
-builder.Build().Run();
